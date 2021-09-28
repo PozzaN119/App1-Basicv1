879c9f48fde4ae48d1d741760737e9bd9e822f04
NPozza119 <noe.pozza@gmail.com>
2021-09-28 22:54:34 +0200

    Test 1

---
+++ src/main/kotlin/GameRound.kt
@@ -0,0 +1,15 @@
+/***
+ * GameRound class represent a round of the game.
+ * It allows to
+ *  - Distribute a name of ...
+ *  - Distribute contrains words
+ *  - Change its state
+ *  - Set a ...
+ *  - Prints words
+ *  - Ends instance
+ */
+
+class GameRound (val player: ArrayList<ArrayList<Int>>) {
+
+
+}
